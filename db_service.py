3d0c7bd6129017b1be02f8fc2e980259cf918d98
Jonathan Feenstra <jonathan.feenstra@upcmail.nl>
2020-10-01 15:24:41 +0200

    Properly close database when shutting down

--- db_service.py
+++ db_service.py
@@ -4,20 +4,18 @@ from aiosqlite import connect
 
 
 class DBService:
-    async def __del__(self):
-        await self.conn.close()
 
     @classmethod
     async def create(cls, config):
         self = DBService()
-        self.conn = await connect('configs/QuoteBot.db', detect_types=PARSE_DECLTYPES | PARSE_COLNAMES)
-        self.c = await self.conn.cursor()
+        self.con = await connect('configs/QuoteBot.db', detect_types=PARSE_DECLTYPES | PARSE_COLNAMES)
+        self.cur = await self.con.cursor()
 
-        result = await (await self.c.execute("PRAGMA auto_vacuum")).fetchone()
+        result = await (await self.cur.execute("PRAGMA auto_vacuum")).fetchone()
         if result[0] == 0:
-            await self.c.execute("PRAGMA auto_vacuum = 1")
+            await self.cur.execute("PRAGMA auto_vacuum = 1")
 
-        await self.c.execute("""
+        await self.cur.execute("""
             CREATE TABLE
             IF NOT EXISTS guild (
                 id INTEGER PRIMARY KEY,
@@ -27,7 +25,7 @@ class DBService:
                 pin_channel INTEGER
             )
         """.format(config['default_prefix'], config['default_lang']))
-        await self.c.execute("""
+        await self.cur.execute("""
             CREATE TABLE
             IF NOT EXISTS personal_quote (
                 id INTEGER PRIMARY KEY,
@@ -38,7 +36,10 @@ class DBService:
         return self
 
     async def execute(self, sql: str, *args):
-        return await self.c.execute(sql, *args)
+        return await self.cur.execute(sql, *args)
 
     async def commit(self):
-        return await self.conn.commit()
+        return await self.con.commit()
+
+    async def close(self):
+        return await self.con.close()
