256f67e1f29988c9a8e2a051e2afdf8da3d9bb32
Deivedux <deivedux-socialmedia@protonmail.com>
2020-10-11 14:03:22 +0300

    Missed a localized string

--- cogs/Main.py
+++ cogs/Main.py
@@ -17,11 +17,11 @@ class Main(commands.Cog):
         embed.set_author(name=str(msg.author), icon_url=msg.author.avatar_url)
         if msg.attachments:
             if msg.channel.is_nsfw() and not channel.is_nsfw():
-                embed.add_field(name='Attachment(s)', value=f":underage: {await self.bot.localize(guild, 'MAIN_quote_nonsfw')}")
+                embed.add_field(name=f"{await self.bot.localize(guild, 'MAIN_quote_attachments')}", value=f":underage: {await self.bot.localize(guild, 'MAIN_quote_nonsfw')}")
             elif len(msg.attachments) == 1 and (url := msg.attachments[0].url).lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.gifv', '.webp', '.bmp')):
                 embed.set_image(url=url)
             else:
-                embed.add_field(name='Attachment(s)', value='\n'.join(f'[{attachment.filename}]({attachment.url})' for attachment in msg.attachments))
+                embed.add_field(name=f"{await self.bot.localize(guild, 'MAIN_quote_attachments')}", value='\n'.join(f'[{attachment.filename}]({attachment.url})' for attachment in msg.attachments))
         footer_text = await self.bot.localize(guild, 'MAIN_quote_embedfooter')
         embed.set_footer(text=footer_text.format(str(user), msg.channel.name))
         return embed
