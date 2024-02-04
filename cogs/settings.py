14bf0e4ce42892fdd2103f16ceffe74ccbfdee04
Jonathan Feenstra <26406078+JonathanFeenstra@users.noreply.github.com>
2024-02-04 19:46:12 +0100

    Run black

--- cogs/settings.py
+++ cogs/settings.py
@@ -14,6 +14,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 from discord.ext import commands
 
 from bot import QuoteBot
